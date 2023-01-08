168625bfb6a5ae140304ab570c1d5709ac32dc8a
4010 <osmium18452@gmail.com>
2023-01-08 22:55:27 +0800

    no comment

--- datapreprocess.py
+++ datapreprocess.py
@@ -1,3 +1,35 @@
 import os
+import numpy as np
+import pickle
 
-train_path="ServerMachineDataset/train"
+
+def convert_to_float(words):
+    ret = []
+    for i in words:
+        ret.append(float(i))
+    return ret
+
+
+train_path = "ServerMachineDataset/train"
+test_path = "ServerMachineDataset/test"
+label_path = "ServerMachineDataset/test_label"
+
+# file_name = "machine-1-1.txt"
+
+for path_name in (test_path,label_path):
+    for root, path, file_list in os.walk(path_name):
+        for file_name in file_list:
+            f = open(os.path.join(path_name, file_name), "r")
+
+            data = []
+            for line in f.readlines():
+                words = line.strip().split(",")
+                data.append(convert_to_float(words))
+            data = np.array(data)
+
+            f.close()
+            print(data)
+
+            f=open(os.path.join(path_name,file_name[:-4]+".pkl"),"wb")
+            pickle.dump(data,f)
+            f.close()
--- main.py
+++ main.py
@@ -1,2 +1,35 @@
+import os
+
 from causallearn.search.ScoreBased.GES import ges
 
+# Visualization using pydot
+from causallearn.utils.GraphUtils import GraphUtils
+import matplotlib.image as mpimg
+import matplotlib.pyplot as plt
+import io
+import pickle
+import os
+import numpy as np
+
+train_data_path="ServerMachineDataset/train"
+train_data_file_name="machine-1-1.pkl"
+f=open(os.path.join(train_data_path,train_data_file_name),"rb")
+data=pickle.load(f)
+f.close()
+print(data.shape)
+
+X=data
+print("getting graph")
+Record = ges(X, maxP=10)
+print("finished")
+pyd = GraphUtils.to_pydot(Record['G'])
+tmp_png = pyd.create_png(f="png")
+fp = io.BytesIO(tmp_png)
+img = mpimg.imread(fp, format='png')
+plt.axis('off')
+plt.imshow(img)
+plt.show()
+
+#
+# # or save the graph
+# pyd.write_png('simple_test.png')
